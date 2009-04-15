76d4cfa48c77b71f41497abc5405a08ca47408d2
stevenc49 <stevenc49@bb60cac8-25a5-11de-870d-bbf10152ea7e>
2009-04-15 07:18:20 +0000

    login works in test.py

--- gnome-google-agenda/src/test.py
+++ gnome-google-agenda/src/test.py
@@ -7,11 +7,12 @@ a.load()
 
 print a.get_user()
 
-google_calendar = gdata.get_services(a)
+#google_calendar = gdata.get_services(a)
+google_calendar = gdata.get_new_instance(a)
 google_calendar.login()
 
-feed = google_calendar.get_user_calendar_feed()
+#feed = google_calendar.get_user_calendar_feed()
 
-content = 'vou a cu do roberto tomorrow'
+content = 'integrate login and other functions tomorrow'
 google_calendar.insert_quick_add_event(content)
 
