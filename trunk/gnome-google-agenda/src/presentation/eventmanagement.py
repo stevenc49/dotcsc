4d66c23bfd55595ef111ac53bc47ecbb3e558d08
stevenc49 <stevenc49@bb60cac8-25a5-11de-870d-bbf10152ea7e>
2009-04-18 09:41:49 +0000

    Empty Message

--- trunk/gnome-google-agenda/src/presentation/eventmanagement.py
+++ trunk/gnome-google-agenda/src/presentation/eventmanagement.py
@@ -31,11 +31,18 @@ class EventTreeView:
     def append_date(self, date):
         self.store.append([date, '#C3D9FF'])
 
-    def sort_events_by_date(self, events):
+    def order_events_by_date(self, events):
         print 'in sorts()'
 
-        for (key, value) in events.items():
-            print "Key = ", key, " Value = " , value
+        for (key, an_event) in events.items():
+            print "Key = ", key
+            print "an_event.title.text = ", an_event.title.text
+            for a_when in an_event.when:
+                print "a_when.start_time = ", a_when.start_time
+
+#        for (key, an_event) in events.items():
+#            print "Key = ", key
+#            print "an_event.title.text = ", an_event.title.text
 
         # TODO: order-by-date algorithm here
 
