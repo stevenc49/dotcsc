d358fccc6e0f5496e48113e23fea7cb500a004dd
stevenc49 <stevenc49@bb60cac8-25a5-11de-870d-bbf10152ea7e>
2009-04-19 00:59:00 +0000

    "tree_view_list" completed, just need to integrate with UI

--- gnome-google-agenda/src/presentation/eventmanagement.py
+++ gnome-google-agenda/src/presentation/eventmanagement.py
@@ -1,3 +1,6 @@
+import time
+import re
+
 import pygtk
 import gtk
 
@@ -32,30 +35,55 @@ class EventTreeView:
         self.store.append([date, '#C3D9FF'])
 
     def order_events_by_date(self, events):
-        print 'in sorts()'
+        
+        # use a Set datatype to get unique event dates
+        dates_list = []
 
         for (key, an_event) in events.items():
-            print "Key = ", key
-            print "an_event.title.text = ", an_event.title.text
             for a_when in an_event.when:
-                print "a_when.start_time = ", a_when.start_time
+                dates_list.append(a_when.start_time)
 
-#        for (key, an_event) in events.items():
-#            print "Key = ", key
-#            print "an_event.title.text = ", an_event.title.text
+        dates_sets = set(dates_list)
 
-        # TODO: order-by-date algorithm here
+        # for each item in the Set, add to the tree_view_list
+        tree_view_list = []
 
-    def load(self):
-        self.append_date('Today Dec 7')
-        self.append_event('Fazer compras')        
-        self.append_event('Arroz')
-        self.append_event('8pm - Aula de cena')
-        
-        self.append_date('Tomorrow Dec 8')
-        self.append_event('Teste de SAD')
-        
-        self.view.show()
+        while( not len(dates_sets) == 0 ):            
+            tree_view_list.append( dates_sets.pop() )
+
+        # for each event, add it to the list, after its start_time
+        for (key, an_event) in events.items():
+            for a_when in an_event.when:
+                events_date_index = tree_view_list.index( a_when.start_time ) + 1
+
+                tree_view_list.insert( events_date_index, an_event.title.text )
+
+        return tree_view_list
+
+#        for item in tree_view_list:
+#            print item
+
+#        datestring = "2009-04-18"
+#        c = time.strptime(datestring,"%Y-%m-%d")
+#        print time.strftime("%d %b %Y",c)
+
+    def load(self, tree_view_list):
+
+        date_regex = re.compile("\d\d\d\d-\d\d-\d\d")
+
+        for item in tree_view_list:
+            if date_regex.match(item):
+                print item
+
+#        self.append_date('Today Dec 7')
+#        self.append_event('Fazer compras')
+#        self.append_event('Arroz')
+#        self.append_event('8pm - Aula de cena')
+#
+#        self.append_date('Tomorrow Dec 8')
+#        self.append_event('Teste de SAD')
+#
+#        self.view.show()
 
     def get_widget(self):
         return self.view
