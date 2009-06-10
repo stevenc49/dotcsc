de815acc020f5d6b39ed48b6788742e6d42b74a4
stevenc49 <stevenc49@bb60cac8-25a5-11de-870d-bbf10152ea7e>
2009-06-10 15:12:48 +0000

    used algorithmist's suggestions for odd (still need 9,99,999 overflow case)

--- exercises/spoj/palin2/palin2.c
+++ exercises/spoj/palin2/palin2.c
@@ -5,13 +5,14 @@
 #define MIL 1000000
 
 char* getPalin(char* num);
+int comp(char* str1, char* str2);
+char* work(char* num, int pos1, int pos2);
 
 int main()
 {
     char* num = (char*)malloc(MIL*sizeof(char));
 
-    //num = "123456";    //read-only
-    strncpy(num, "10804", MIL);
+    strncpy(num, "999", MIL);
 
     printf("\noriginal %s\n",num );
     printf("\npalin %s\n", getPalin(num) );
@@ -23,116 +24,71 @@ char* getPalin(char* num)
 {
     int length = strlen(num);
 
-    if( length%2 == 0 )
+    //save old num
+    char* oldnum = malloc(MIL*sizeof(char));
+    strncpy(oldnum, num, MIL);
+
+    //set pointers
+    char* front = num;
+    char* end = num + strlen(num)-1;    //pointer to end
+
+    //while pointers are not mid, mirror
+    while( front != num+(length/2))
     {
-        printf("even\n");
-
-        char* front = num;
-        char* end = num + strlen(num) - 1;    //pointer to end
-
-        //pointers not middle yet
-        while( front != num + (length/2)-1 )
-        {
-            *end = *front;
-
-            front++;
-            end--;
-        }
-
-        //pointers at mid
-        char mid[3];
-        mid[0] = *front;
-        mid[1] = *end;
-        mid[2] = '\0';
-
-        int i = atoi(mid);
-
-        if(i>=0 && i<11) {
-            *front = '1';
-            *end ='1';
-        } else if (i>=11 && i<22) {
-            *front = '2';
-            *end ='2';
-        } else if (i>=22 && i<33) {
-            *front = '3';
-            *end ='3';
-        } else if (i>=33 && i<44) {
-            *front = '4';
-            *end ='4';
-        } else if (i>=44 && i<55) {
-            *front = '5';
-            *end ='5';
-        } else if (i>=55 && i<66) {
-            *front = '6';
-            *end ='6';
-        } else if (i>=66 && i<77) {
-            *front = '7';
-            *end ='7';
-        } else if (i>=77 && i<88) {
-            *front = '8';
-            *end ='8';
-        } else if (i>=88 && i<99) {
-            *front = '9';
-            *end ='9';
-        } else if (i=99)
-            printf("i=99, need to carry over");
-    } else {
-        printf("odd\n");
-
-        char* front = num;
-        char* end = num + strlen(num)-1;    //pointer to end
-
-        //pointers not middle yet or not next to middle
-        while( front != num+(length/2) && end != num+(length/2)+1 )
-        {
-            *end = *front;
-
-            front++;
-            end--;
-        }
-
-        //pointers next to middle (save values next to mid for comparison later)
-        char mid_plus_one;
-        mid_plus_one = *end;
-
-        char mid_minus_one;
-        mid_minus_one = *front;
-
         *end = *front;
+
         front++;
         end--;
+    }
 
-        //pointers at mid
-        char mid;
-        mid = *front;
+    if (comp(oldnum, num)==0)
+    {
+        int pos1 = front - num;
+        int pos2 = end - num;
 
-        if ( mid_plus_one > *(front+1))            //if old value > new value for mid_plus_one
-        {                                           //then increment middle digit
-            if(mid=='0') {
-                *front = '1';
-            } else if (mid=='1') {
-                *front = '2';
-            } else if (mid=='2') {
-                *front = '3';
-            } else if (mid=='3') {
-                *front = '4';
-            } else if (mid=='4') {
-                *front = '5';
-            } else if (mid=='5') {
-                *front = '6';
-            } else if (mid=='6') {
-                *front = '7';
-            } else if (mid=='7') {
-                *front = '8';
-            } else if (mid=='8') {
-                *front = '9';
-            } else if (mid=='9') {  //false case: num=19994, 10904, 10804
-                *front = '0';
-                *(front-1) = mid_minus_one + 1;
-                *(end+1) = mid_minus_one + 1;
-            }
-        }
+        num = work(num, pos1, pos2);
     }
 
     return num;
+}
+
+//increments to next palindrome
+//precondition: needs to be a palindrome,
+//              pos1 and pos2 must be the middle digits (or middle digit if odd)
+char* work(char* num, int pos1, int pos2)
+{
+    if (num[pos1] < '0')
+    {
+        printf("overflow\n");
+    }
+    else if (num[pos1] < '9')
+    {
+        num[pos1] = num[pos2] = num[pos1] + 1;
+    }
+    else
+    {
+        num[pos1] = num[pos2] = 0;
+        work(num, pos1-1, pos2+1);
+    }
+
+    return num;
+}
+
+//returns true if str2 > str1
+int comp(char* str1, char* str2)
+{
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
+
+    int i;
+
+    if( len1 != len2 )
+        return len1 < len2;
+    for (i=0; i<len1; i++)
+    {
+        if (str1[i] != str2[i])
+            return str1[i] < str2[i];
+    }
+
+    return 0;
 }
